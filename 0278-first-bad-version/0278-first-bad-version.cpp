2da25373faa0daf34e1304c11019dbf19b46e2ff
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-06-15 17:20:48 +0530

    Time: 0 ms (100%), Space: 7.9 MB (37.34%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -1,12 +1,17 @@
-class Solution{
+class Solution {
 public:
     int firstBadVersion(int n) {
-        int l = 0, h = n;
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-            if (isBadVersion(mid)) h = mid - 1;
-            else l = mid + 1;
+        int start=1;
+        int end=n;
+        int ans=-1;
+        while (start<=end){
+            int mid=start+(end -start )/2;
+            if (isBadVersion(mid)){
+                ans=mid;
+                end=mid-1;
+            }
+            else start=mid+1;
         }
-        return l;
+        return ans;
     }
 };
