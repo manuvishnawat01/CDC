8e7754bbfffdf18c783f3f58f4c1428e11b3bb4f
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-08-23 00:34:02 +0530

    Time: 12 ms (10.66%), Space: 26.6 MB (5%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
-        vector<vector<int>> ans;
-        for(auto i : intervals){
-            if(ans.empty() || ans.back()[1] < i[0]){
-                ans.push_back(i);
-            }
-            else{
-                ans.back()[1] = max(ans.back()[1] , i[1]);
-            }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) 
+    {
+       vector<pair<int,int>>vec;    
+       vector<vector<int>>ans;
+       for(auto it:intervals){
+        vec.push_back({it[0],it[1]});
+       }
+
+       sort(vec.begin(),vec.end());
+       int fir=vec[0].first;
+       int sec=vec[0].second;
+
+       for(int i=0;i<vec.size()-1;i++){
+        if(sec>=vec[i+1].first){
+            sec=max(sec,vec[i+1].second);   
         }
-        return ans;
+
+        else{
+               vector<int>temp;
+               temp.push_back(fir);
+               temp.push_back(sec);
+
+               fir=vec[i+1].first;
+               sec=vec[i+1].second;
+               ans.push_back(temp);
+        }
+       }
+
+       ans.push_back({fir,sec});       
+       return ans;
     }
 };
