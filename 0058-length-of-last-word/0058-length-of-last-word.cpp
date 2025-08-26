c9fb7f34e30f7e3fe2092f6f92cd79f339da0cfb
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-08-27 01:05:32 +0530

    Time: 0 ms (100%), Space: 9 MB (9.51%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n = s.length();
-        int i = n-1;
-        while (s[i] == ' ' && n >= 0){
-            i--;
+        int count = 0;
+        bool flag = false;
+        for(int i = s.size()-1; i>=0; i--){
+            if(s[i] != ' '){
+                count++;
+                flag = true;
+            }
+            else if(flag == true){
+                break;
+            }
         }
-        int start = i;
-        while(start >= 0 && s[start] != ' '){
-            start--;
-        }
-        return i-start;
+        return count;
     }
 };
