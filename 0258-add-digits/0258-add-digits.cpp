3e5935513458a83b0562bab7ce5a6ab02ee8698a
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-08-28 10:37:16 +0530

    Time: 0 ms (100%), Space: 8.5 MB (77.94%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int addDigits(int num) {
-      int sum=0;
-      int ld;
-        while(num){
-            ld=num%10;
-            sum=sum+ld;
-            num/=10;
+    int addDigits(int nums) {
+        while(nums>=10){
+            int sum = 0;
+            while(nums>0){
+            int ld=nums%10;
+            sum = sum +ld;
+            nums=nums/10;
+            }
+            nums = sum;
         }
-        if(sum<10)
-            return sum;
-        else
-            return addDigits(sum);
+        return nums;
     }
 };
