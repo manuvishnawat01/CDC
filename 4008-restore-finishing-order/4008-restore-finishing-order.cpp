281dc21ad66f5738b9d8e4d47e4738d50eb04474
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-09-04 01:48:49 +0530

    Time: 3 ms (39.33%), Space: 50.6 MB (19.85%) - LeetHub

--- 4008-restore-finishing-order/4008-restore-finishing-order.cpp
+++ 4008-restore-finishing-order/4008-restore-finishing-order.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
         vector<int> ans;
+        unordered_set<int> s ;
+        for(int i=0; i<friends.size(); i++){
+            s.insert(friends[i]);
+        }
+
         for(int i = 0; i<order.size(); i++){
-            int  a = order[i];
-            for(int j = 0; j<friends.size(); j++){
-                if(a == friends[j]) ans.push_back(a);
-            }
+            int a = order[i];
+            if(s.find(a) != s.end())  ans.push_back(order[i]) ;
         }
         return ans;
     }
