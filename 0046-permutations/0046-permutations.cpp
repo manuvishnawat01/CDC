6c46cadb4e068dd5dc84ada4a9851d2339334ca9
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-09-27 22:31:19 +0530

    Time: 0 ms (100%), Space: 10.7 MB (47.3%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    void getperms(vector<int>& nums ,int idx, vector<vector<int>> &ans){
-        if(idx == nums.size()){
-            ans.push_back({nums});
+
+    void permut(vector<int> &arr,vector<vector<int>> &ans, int index){
+        if(index == arr.size()){
+            ans.push_back(arr);
             return;
         }
-
-        for(int i =idx; i<nums.size(); i++){
-            swap(nums[idx],nums[i]);
-            getperms(nums,idx+1,ans);
-
-            swap(nums[idx],nums[i]);
+        for(int  i= index; i<arr.size(); i++){
+            swap(arr[i],arr[index]);
+            permut(arr,ans,index+1);
+            swap(arr[i],arr[index]);
         }
     }
-
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        getperms(nums,0,ans);
+        permut(nums,ans,0);
         return ans;
     }
 };
