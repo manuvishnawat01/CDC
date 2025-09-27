ecc2e79bdb42ce618ab6a49af92f93e62fd43178
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-09-27 23:42:47 +0530

    Time: 0 ms (100%), Space: 12.5 MB (31.87%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-
-    void solve(vector<int>&nums,int i,vector<int>&temp){
-        int n = nums.size();
-        // base case
-        if(i>=n){
+    void subsequence(vector<int> &arr, int index ,int n, vector<vector<int>> &ans, vector<int> &temp){
+        if(index == n){
             ans.push_back(temp);
-            return;
+            return ;
         }
-
-        //recursion
-        temp.push_back(nums[i]); // take
-        solve(nums,i+1,temp);
-        temp.pop_back(); // not take
-        solve(nums,i+1,temp);
+        subsequence(arr,index+1,n,ans,temp);
+        temp.push_back(arr[index]);
+        subsequence(arr,index+1,n,ans,temp);
+        temp.pop_back();
     }
-
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> temp;
-        solve(nums,0,temp);
-
-        return ans;
+    vector<vector<int>> ans;
+    vector<int> temp;
+    subsequence(nums,0,nums.size(),ans,temp);    
+    return ans;
     }
 };
