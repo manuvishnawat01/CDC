00345cbfc004bfec9de0900334bb484159bb1004
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-10-18 13:38:00 +0530

    Time: 2 ms (43.65%), Space: 14.6 MB (17.59%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int>  s;
         vector<int> ans;
-        for(int i =0; i<nums1.size(); i++){
-            s.insert(nums1[i]);
+        unordered_map<int,int> mp;;
+        for(auto n : nums1){
+            mp[n]++;
         }
-        for(int j =0; j<nums2.size(); j++){
-            if(s.find(nums2[j]) != s.end()){
-                ans.push_back(nums2[j]);
-                s.erase(nums2[j]);
+
+        for(auto n : nums2){
+            if(mp[n]>0){
+            ans.push_back(n);
+            mp[n] = 0;
             }
         }
         return ans;
