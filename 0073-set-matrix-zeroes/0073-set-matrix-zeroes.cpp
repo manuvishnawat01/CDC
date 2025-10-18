571d4c117da8c5a33275766f67a26430755ac2e6
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-10-18 14:38:49 +0530

    Time: 0 ms (100%), Space: 18.8 MB (10.83%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,24 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-        vector<int> row(n, 0); 
-        vector<int> col(m, 0); 
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(matrix[i][j] == 0) {
-                    row[i] = 1; 
-                    col[j] = 1; 
+        unordered_set<int> rows;
+        unordered_set<int> cols;
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.insert(i);
+                    cols.insert(j);
                 }
             }
         }
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(row[i] == 1 || col[j] == 1) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rows.count(i) || cols.count(j)) {
                     matrix[i][j] = 0;
                 }
             }
