87db328217121984d72d78ae7a243cce0df1339d
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-11-01 15:51:41 +0530

    Time: 0 ms (100%), Space: 13.5 MB (39%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,15 +10,14 @@
  */
 class Solution {
 public:
+
+    ListNode* reverse(ListNode* curr,ListNode* prev){
+        if(curr == NULL) return prev;
+        ListNode* fut = curr->next;
+        curr->next = prev;
+        return reverse(fut,curr);
+    }
     ListNode* reverseList(ListNode* head) {
-        ListNode *curr = head, *prev = NULL, *fut = NULL;
-        while(curr){
-            fut = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = fut;
-        }
-        head = prev;
-        return head;
+        return reverse(head,NULL);
     }
 };
