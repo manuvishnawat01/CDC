506b19404c1d7437b91350ce5ff80c6a2da52ea5
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-11-03 09:33:18 +0530

    Time: 0 ms (100%), Space: 8.7 MB (33.1%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        if(n>m) return false;
+        int i = 0;
+        int j = 0;
+        while(j<m){
+            if(s[i] == t[j]){
+                i++;
+                j++;
+            }
+            else if(s[i] != t[j]) j++;
+        }
+        return i==n;
+    }
+};
