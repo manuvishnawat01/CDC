abdae14523627bccf7df4f4df6f1a2452df12f07
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-11-05 23:36:12 +0530

    Time: 0 ms (100%), Space: 13 MB (76.88%) - LeetHub

--- 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.cpp
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.cpp
@@ -1,13 +1,15 @@
-class Solution{
+class Solution {
 public:
     int mostWordsFound(vector<string>& sentences){
         int maxi = 0;
-        for(string &s : sentences){
+        for(int i = 0; i < sentences.size(); i++){
+            int j = 0;
             int count = 1; 
-            for(char c : s){
-                if (c == ' ') count++;
+            while(j < sentences[i].size()){
+                if(sentences[i][j] == ' ') count++;
+                j++;
             }
-            maxi = max(maxi,count);
+            maxi = max(count,maxi);
         }
         return maxi;
     }
