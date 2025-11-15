4d4d2e0e0e6551884556fda88e82007708e2e2a4
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-11-15 19:24:15 +0530

    Time: 0 ms (100%), Space: 14.8 MB (83.61%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -15,24 +15,15 @@ public:
         if(root == NULL) return 0;
         return 1+max(levels(root->left),levels(root->right));
     }
-    void nthlevel(TreeNode* root,int curr,int level,vector<int>& ans){
-        if(root == NULL) return; 
-        if(curr == level){
-            ans[curr] = root->val;
-            return;
-        }
-        nthlevel(root->left,curr+1,level,ans);
-        nthlevel(root->right,curr+1,level,ans);
-    }
-    void levelorder(TreeNode* root,vector<int>& ans){
-        int n = ans.size();
-        for(int i = 0; i<n; i++){
-            nthlevel(root,0,i,ans);
-        }
+    void preorder(TreeNode* root, vector<int>& ans, int level){
+        if(root == NULL) return;
+        ans[level] = root->val;
+        preorder(root->left,ans,level+1);
+        preorder(root->right,ans,level+1);
     }
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans(levels(root),0);
-        levelorder(root,ans);
+        preorder(root,ans,0);
         return ans;
     }
 };
