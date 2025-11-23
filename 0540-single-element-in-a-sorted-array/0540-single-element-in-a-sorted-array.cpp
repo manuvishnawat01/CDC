eb749ca4c518b4a488b81d2f51eca24b33fb37f2
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-11-23 12:25:53 +0530

    Time: 11 ms (4.24%), Space: 31.3 MB (7.42%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto n : nums) {
+            mp[n]++;
+        }
+        for(auto m : mp){
+            if(m.second == 1) return m.first;
+        }
+        return 0;
+    }
+};
