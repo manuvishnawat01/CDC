ca5b1548bf7fd8123dc21bca1d6138af48b92d4a
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-12-17 00:00:16 +0530

    Time: 4 ms (5.61%), Space: 19.1 MB (5.56%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,29 +1,31 @@
-#include <bits/stdc++.h>
-using namespace std;
 
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        unordered_set<int> rows;
-        unordered_set<int> cols;
-
-        int m = matrix.size();
-        int n = matrix[0].size();
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    rows.insert(i);
-                    cols.insert(j);
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> visited = matrix;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    for(int k=0; k<m; k++){
+                        visited[i][k] = 0;
+                    }
                 }
             }
         }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (rows.count(i) || cols.count(j)) {
-                    matrix[i][j] = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    for(int k=0; k<n; k++){
+                        visited[k][j] = 0;
+                    }
                 }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                matrix[i][j] = visited[i][j];
             }
         }
     }
