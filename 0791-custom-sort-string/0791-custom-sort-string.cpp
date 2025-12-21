937f6e8e531991c6e1c16a1e4630905e15c272b6
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-12-21 13:01:39 +0530

    Time: 4 ms (1.83%), Space: 9.6 MB (5.48%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        unordered_map<char,int> mp;
+        for(char c : s) mp[c]++;
+
+        string ans;
+
+        for(char c : order){
+            for(int i = 0; i< mp[c]; i++){
+                ans = ans+c;
+            }
+            mp[c] = 0;
+        }
+
+        for(char c : s){
+            if(mp[c] > 0) ans = ans+c;
+        }
+        return ans;
+    }
+};
