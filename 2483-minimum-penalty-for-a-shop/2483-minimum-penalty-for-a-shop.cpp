4d439587fda4ac2818a75e6ee935f6f659a16e40
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-12-26 21:56:46 +0530

    Time: 8 ms (55.08%), Space: 13.1 MB (98.36%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int totalY = 0;
+        for(char c : customers){
+            if (c == 'Y') totalY++;
+        }
+
+        int openPenalty = 0;    
+        int closedPenalty = totalY; 
+
+        int minPenalty = closedPenalty;
+        int bestHour = 0;
+
+        for (int i = 0; i < customers.size(); i++){
+            if(customers[i] == 'N') openPenalty++;
+            else closedPenalty--;
+
+            int currentPenalty = openPenalty + closedPenalty;
+
+            if(currentPenalty < minPenalty){
+                minPenalty = currentPenalty;
+                bestHour = i + 1;
+            }
+        }
+        return bestHour;
+    }
+};
