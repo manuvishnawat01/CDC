99c2bc64ee7ff8319117f18050fd7055050e2ed3
Manu Pratap Singh <csai23176@glbitm.ac.in>
2025-12-27 21:05:10 +0530

    Time: 0 ms (100%), Space: 10.1 MB (34.94%) - LeetHub

---
+++ 0932-beautiful-array/0932-beautiful-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> beautifulArray(int n) {
+
+        vector<int> ans = {1};
+        while(ans.size() < n){
+            vector<int> temp;
+            for(int x : ans){
+                if (2 * x - 1 <= n)
+                    temp.push_back(2 * x - 1);
+            }
+            for(int x : ans){
+                if (2 * x <= n)
+                    temp.push_back(2 * x);
+            }
+            ans = temp;
+        }
+        return ans;
+    }
+};
