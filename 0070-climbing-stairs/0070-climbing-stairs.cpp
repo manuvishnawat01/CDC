41859923a47e7824835ed648c016db3b264e2cca
Manu Pratap Singh <csai23176@glbitm.ac.in>
2026-01-24 10:36:11 +0530

    Time: 0 ms (100%), Space: 8.6 MB (17.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        int prev1 = 1;
-        int prev2  =1;
+    int solve(vector<int> &dp,int n){
+        if(n == 0 || n ==1) return 1;
+        dp[0] = 1;
+        dp[1] = 1;
 
-        for(int i =2; i<=n;i++){
-            int c =prev1+prev2;
-            prev2 = prev1;
-            prev1 = c;
+        for(int i = 2 ; i<=n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
         }
-        return prev1;
+
+        return dp[n];
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(dp,n); 
     }
 };
