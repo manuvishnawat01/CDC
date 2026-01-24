6f8c2877a38f50aaab238da50359006aaa80e516
Manu Pratap Singh <csai23176@glbitm.ac.in>
2026-01-24 22:39:30 +0530

    Time: 0 ms (100%), Space: 10.5 MB (62%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
+    int robbery(int index, vector<int>& nums, vector<int>& dp){
+        if(index == 0) return nums[0];
+        if(index == 1) return max(nums[0],nums[1]);
+        if(dp[index] != -1) return dp[index];
+        return dp[index] = max(nums[index]+robbery(index-2,nums,dp) , robbery(index-1,nums,dp));
+    }
     int rob(vector<int>& nums) {
-        int rob = 0;
-        int norob = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int newRob = norob + nums[i];
-            int newNoRob = max(norob, rob);
-            rob = newRob;
-            norob = newNoRob;
-        }
-        return max(rob, norob);
+        int n = nums.size();
+        vector<int> dp(n,-1);
+        return robbery(n-1,nums,dp);
+    
     }
 };
