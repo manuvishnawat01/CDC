6771c86d7a8fde02e089f300dbc01f6c2af3ed74
Manu Pratap Singh <csai23176@glbitm.ac.in>
2026-02-06 22:23:00 +0530

    Time: 0 ms (100%), Space: 52.4 MB (43.57%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        int maxReach = 0;
         int n = nums.size();
-        int a = 0;
-        for(int i = 0; i<n; i++){
-            if(i>a) return false;
-            a = max(a,i+nums[i]);
-            if(a>=n-1) return true;
+
+        for(int i = 0; i < n; i++){
+            if(i > maxReach)  return false;
+            maxReach = max(maxReach, i + nums[i]);
+            if(maxReach >= n - 1)  return true;
         }
         return true;
     }
