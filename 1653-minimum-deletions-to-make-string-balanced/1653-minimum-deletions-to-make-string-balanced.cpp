f26fdcf060b97d83816501a31f888f8d17dd85a3
Manu Pratap Singh <csai23176@glbitm.ac.in>
2026-02-07 21:54:38 +0530

    Time: 12 ms (88.17%), Space: 25.6 MB (72.34%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n=s.size(),count_b=0,res=0;
-        for (int i=0;i<n;i++) {
-            if (s[i]=='b') {
-                count_b++;
-            }
-            else {
-                if (count_b>0) {
-                    res++;
-                    count_b--;
-                }
-            }
+        int bCount = 0;
+        int deletions = 0;
+
+        for (char c : s) {
+            if(c == 'b') bCount++;
+            else deletions = min(deletions + 1, bCount);
         }
-        return res;
+        return deletions;
     }
 };
