af7da2ca0dfbf5f88fb758dad1f6ca92da3b6461
Manu Pratap Singh <csai23176@glbitm.ac.in>
2026-02-13 09:49:04 +0530

    Time: 4 ms (21.33%), Space: 17 MB (85.28%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,31 +1,26 @@
-
 class Solution {
-
-    long resolves(int a, int b, char Operator){
-        if(Operator == '+') return a + b;
-        else if(Operator == '-') return a - b;
-        else if(Operator == '*') return (long)a*b;
-        return a/b;
-    }
 public:
-    int evalRPN(vector<string>& tokens) {
-        stack<long> Stack;
-        int n = tokens.size();
-        for(int i = 0; i < n; i++){
+    bool isOperator(const string& c) {
+        return (c == "+" || c == "-" || c == "*" || c == "/");
+    }
 
-            if(tokens[i].size() == 1 and tokens[i][0] < 48){
-                long integer2 = Stack.top();
-                Stack.pop();
-                long integer1 = Stack.top();
-                Stack.pop();
-                
-                string Operator = tokens[i];
-                long resolvedAns = resolves(integer1, integer2 , Operator[0]);
-                Stack.push(resolvedAns);
-            }else 
-                Stack.push(stol(tokens[i]));
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for(auto c:tokens){
+            if(isOperator(c)){
+                int a = st.top();
+                st.pop();
+                int b = st.top();
+                st.pop();
+                if(c == "+") st.push(b+a);
+                else if(c == "-") st.push(b-a);
+                else if(c == "*") st.push(b*a);
+                else st.push(b/a);
+             }
+            else{
+                st.push(stoi(c));
+            }
         }
-        return Stack.top();
+        return st.top();
     }
 };
-
