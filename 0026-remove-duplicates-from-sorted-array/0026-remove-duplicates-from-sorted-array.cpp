12b02982a09c0610ed1cb9003e9cc42ee86f474c
Manu Pratap Singh <csai23176@glbitm.ac.in>
2026-03-28 12:23:12 +0530

    Time: 0 ms (100%), Space: 22.7 MB (51.95%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
-        int unique = 1;
-        
-        for(int i=1; i<nums.size(); i++)
-            if(nums[i] != nums[i-1])
-                nums[unique++] = nums[i];
-        
-        return unique;
+        if(nums.empty()) return 0;
+        int k = 1; 
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i]; 
+                k++;
+            }
+        }
+        return k;
     }
 };
