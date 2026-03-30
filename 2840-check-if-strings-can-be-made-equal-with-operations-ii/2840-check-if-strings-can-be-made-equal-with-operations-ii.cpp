0cbf587f50b030b152054f1a095bd25c428646e4
Manu Pratap Singh <csai23176@glbitm.ac.in>
2026-03-31 00:11:09 +0530

    Time: 2 ms (95.4%), Space: 19.9 MB (58.05%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> even1(26, 0), odd1(26, 0);
         vector<int> even2(26, 0), odd2(26, 0);
         
-        for (int i = 0; i < s1.length(); ++i) {
+        for (int i = 0; i < s1.size(); i++) {
             if (i % 2 == 0) {
                 even1[s1[i] - 'a']++;
                 even2[s2[i] - 'a']++;
@@ -14,7 +14,6 @@ public:
             }
         }
         
-        return even1 == even2 && odd1 == odd2;
-
+        return (even1 == even2) && (odd1 == odd2);
     }
 };
