2f2646b683c2b267bb1c16badb8f2b22d051d729
Manu Pratap Singh <csai23176@glbitm.ac.in>
2026-03-31 01:35:02 +0530

    Time: 0 ms (100%), Space: 10.2 MB (69.16%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
+    void transpose(vector<vector<int>>& matrix, int n){
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
+    }
 
-        for(int i = 0; i < n; i++) {
-            reverse(matrix[i].begin(), matrix[i].end());
+    void reverseRows(vector<vector<int>>& matrix, int n){
+        for(int i = 0; i < n; i++){
+            int left = 0, right = n - 1;
+            while(left < right){
+                swap(matrix[i][left], matrix[i][right]);
+                left++;
+                right--;
+            }
         }
+    }
+
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        transpose(matrix, n);
+        reverseRows(matrix, n); 
     }
 };
