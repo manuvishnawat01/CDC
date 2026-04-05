122a8bcd13751a7da788bf10d50b3feadfb10447
Manu Pratap Singh <csai23176@glbitm.ac.in>
2026-04-05 11:27:22 +0530

    Time: 0 ms (100%), Space: 10.5 MB (13.45%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        int left = 0;
-        int right = 0;
-        int up = 0;
-        int down  = 0;
-        for(auto m : moves){
-            if(m == 'L') left++;
-            else if(m == 'R') right++;
-            else if(m == 'U') up++;
-            else down++;
-        }
-
-        if(left == right && up == down) return true;
+        unordered_map<char, int> mp;
+        for(auto m : moves) mp[m]++;
+        
+        if(mp['L'] == mp['R'] && mp['U'] == mp['D']) return true;
         return false;
     }
 };
