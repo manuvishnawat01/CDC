194913ade2e646ee566e9abbcc8df0af6aeb72d6
Manu Pratap Singh <csai23176@glbitm.ac.in>
2026-04-07 00:16:03 +0530

    Time: 0 ms (100%), Space: 7.6 MB (93.44%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
     bool canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
-        return targetCapacity == 0 || 
+        return targetCapacity == 0 ||
                (jug1Capacity + jug2Capacity >= targetCapacity &&
-                targetCapacity % gcd(jug1Capacity, jug2Capacity) == 0);
-    }
-
-private:
-    int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
+                targetCapacity % __gcd(jug1Capacity, jug2Capacity) == 0);
     }
 };
