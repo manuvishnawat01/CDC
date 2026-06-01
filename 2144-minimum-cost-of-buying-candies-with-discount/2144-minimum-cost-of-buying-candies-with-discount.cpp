fcb6c7a188f110d6aada253ff511977bd7debd5c
Manu Pratap Singh <manu.pratap2004@gmail.com>
2026-06-01 11:24:56 +0530

    Time: 4 ms (5.99%), Space: 16.9 MB (10.3%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -3,12 +3,10 @@ public:
     int minimumCost(vector<int>& cost) {
         sort(cost.rbegin(), cost.rend());
         int ans = 0;
-        for (int i = 0; i < cost.size(); i += 3) {
-            ans += cost[i];
-            if (i + 1 < cost.size()) {
-                ans += cost[i + 1];
+            for(int i = 0; i < cost.size(); i += 3){
+                ans += cost[i];
+                if(i + 1 < cost.size()) ans += cost[i + 1];
             }
-        }
         return ans;
     }
 };
