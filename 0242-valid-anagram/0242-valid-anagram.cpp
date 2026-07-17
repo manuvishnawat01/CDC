d5bb005fa0cae06de083ad1b5b48598395483579
Manu Pratap Singh <manu.pratap2004@gmail.com>
2026-07-17 18:34:12 +0530

    Time: 8 ms (7.02%), Space: 9.6 MB (76.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        
+        if(s.length() != t.length()) return false;
+
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        if(s.size() != t.size()) return false;
-        for(int i = 0; i<s.size(); i++){
-            if(s[i] != t[i]) return false;
-        }
-        return true;
+
+        return s == t;
     }
 };
