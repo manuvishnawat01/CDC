0aed81c579f9c2ef7fdb08b86543efed8d521f8d
Manu Pratap Singh <manu.pratap2004@gmail.com>
2026-07-17 19:20:18 +0530

    Time: 0 ms (100%), Space: 14.8 MB (38.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i = 0; i< nums.size(); i++){
-            for(int j = i+1; j<nums.size(); j++){
-                if(nums[i]+nums[j] == target) return {i,j};
+        //creating unorderde map.
+
+        unordered_map<int,int> mp;
+
+    
+        for(int i = 0; i<nums.size(); i++){
+            int complement = target-nums[i];
+            // checking if the required complement is already present in the map.
+            //if it is present we directly return pair.
+            if(mp.find(complement) != mp.end()){// this is the way to find element in map.
+                return {mp[complement],i};
             }
+            mp[nums[i]] = i; // increasing the frequency of each element present in map
         }
-        return { };
+        return {};// if no pair found return NULL. or { };
     }
 };
